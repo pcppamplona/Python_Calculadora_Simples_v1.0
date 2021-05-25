c60b6ef0fc459bee43d95b9c6c7438c2911f78e8
Pedro <pedrocp010102@gmail.com>
2021-05-25 09:57:03 -0300

    Agora faz soma com funcao

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,18 @@
+def soma(a, b):
+    return a + b
+
 a = int(input("Digite o primeiro número: "))
 b = int(input("Digite o segundo número: "))
+
 operacao = input("\n+:Soma\n-:Subtração\n*:Subtração\n/:Divisão\n**:expoente\n")
 if operacao == '+':
-    resultado = a + b
-
+    resultado = soma(a, b)
 elif operacao == '-':
     resultado = a - b
-
 elif operacao == '*':
     resultado = a * b
-
 elif operacao == '/':
     resultado = a//b
-
 elif operacao == '**':
     resultado = a ** b
 print(resultado)
