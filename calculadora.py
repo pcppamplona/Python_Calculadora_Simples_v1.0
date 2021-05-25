5f90cb92e8188808f4c7110f4b9bd00d2d5104bf
Pedro <pedrocp010102@gmail.com>
2021-05-25 08:51:28 -0300

    faz as 4 operacoes

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,15 @@
 a = int(input("Digite o primeiro número: "))
 b = int(input("Digite o segundo número: "))
-res = a + b
-print(res)
+operacao = input("\n+:Soma\n-:Subtração\n*:Subtração\n/:Divisão\n")
+if operacao == '+':
+    resultado = a + b
+
+elif operacao == '-':
+    resultado = a - b
+
+elif operacao == '*'
+    resultado = a * b
+
+elif operacao == '/'
+    resultado = a//b
+print(resultado)
